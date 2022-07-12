ef7e5e28b283f20a651d3e614dadcf5010ed3530
Ming6419 <Triancmf@gmail.com>
2022-07-12 23:01:49 +0800

    Add files via upload

--- Covid Status Project.sql
+++ Covid Status Project.sql
@@ -1,27 +1,21 @@
-/*
+
 Select * 
 From Portfolio1.dbo.CovidDeaths
 where continent is null
 order by location, date;
-*/
 
-/*
 Select * 
 From Portfolio1.dbo.CovidVaccinations
 where continent is not null
 order by location, date
-*/
 
-/*
 --Select Data to be used
 
 Select Location, Date, total_cases, new_cases, total_deaths, population
 from Portfolio1.dbo.CovidDeaths
 where continent is not null
 order by location, Date
-*/
 
-/*
 -- Looking at Total Cases vs Total Deaths
 -- Shows covid death percentage as contracted cases.
 
@@ -29,9 +23,7 @@ Select Location, Date, ISNULL(total_cases, 0), ISNULL(total_deaths, 0), (ISNULL(
 from Portfolio1.dbo.CovidDeaths
 where location like 'Malaysia' and continent is not null
 order by location, Date
-*/
 
-/*
 -- Looking at Total Cases vs Population
 -- Shows Covid contraction percentage per population
 
@@ -40,9 +32,7 @@ from Portfolio1.dbo.CovidDeaths
 where continent is not null
 --where location like 'Malaysia'
 order by location, Date
-*/
 
-/*
 -- Looking at Countries with Highest Infection Rate with respect to Population
 -- NUll value are treated as 0 for cases
 
@@ -52,9 +42,7 @@ from Portfolio1.dbo.CovidDeaths
 where continent is not null
 Group by Location, Population
 order by MAXCovidPercentage DESC
-*/
 
-/*
 -- Looking at Countries with Highest Death Count
 -- NUll value are treated as 0 for cases
 
@@ -63,9 +51,7 @@ from Portfolio1.dbo.CovidDeaths
 where continent is not null
 Group by Location
 order by [Highest Total Death Count] DESC
-*/
 
-/*
 -- Looking at Continents with Highest Death Count
 -- remove income type data
 
@@ -74,9 +60,7 @@ from Portfolio1.dbo.CovidDeaths
 where continent is null and location not like '%income%'
 Group by location
 order by [Highest Total Death Count] DESC
-*/
 
-/*
 -- Global numbers with respect to time
 
 Select Date, sum(ISNULL(total_cases, 0)) as [Total Cases Count], sum(ISNULL(cast(total_deaths as float), 0)) as [Total Death Count], (sum(ISNULL(cast(total_deaths as float), 0))/sum(total_cases))*100 as [Death percentage]
@@ -84,9 +68,7 @@ from Portfolio1.dbo.CovidDeaths
 --where location like 'Malaysia' and continent is not null
 group by Date
 order by 1 DESC
-*/
 
-/*
 --Looking at Total polulation vs Vaccination
 
 Select dea.continent, dea.location, dea.date, dea.population, isnull(vac.new_vaccinations,0) as new_vaccinations
@@ -97,9 +79,7 @@ join Portfolio1.dbo.CovidVaccinations vac
 	and dea.date = vac.date
 where dea.continent is not null and dea.location not like '%income%'
 order by 2,3
-*/
 
-/*
 -- USE CTE
 
 With PopvsVac (Continent, Location, Date, Population, new_vaccination, Cummulative_vaccinations)
@@ -117,9 +97,7 @@ where dea.continent is not null and dea.location not like '%income%'
 Select *, (Cummulative_vaccinations/population)*100 as Vaccination_Percentage
 From PopvsVac
 Order by 2,3
-*/
 
-/*
 --Temp table
 
 Drop Table if exists #PercentagePopulationVaccinated
@@ -145,9 +123,7 @@ where dea.continent is not null and dea.location not like '%income%'
 
 Select *, (Cummulative_vaccinations/population)*100 as Vaccination_Percentage
 from #PercentagePopulationVaccinated
-*/
 
-/*
 -- Creating View to store data for later visualisations.
 
 drop view if exists PercentagePopulationVaccinated
@@ -216,6 +192,6 @@ CREATE VIEW Covid_DeathCasesPerCountry as
 Select Location, Date, total_cases, new_cases, total_deaths, population
 from Portfolio1.dbo.CovidDeaths
 where continent is not null
-*/
+
 
 
